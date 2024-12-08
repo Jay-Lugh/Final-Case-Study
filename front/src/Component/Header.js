d2d7d34541b9944e80d45a985f566d3d6fc72a3f
Jay-Lugh <jaylugh23@gmail.com>
2024-12-09 01:11:18 +0800

    Change in Header

--- front/src/Component/Header.js
+++ front/src/Component/Header.js
@@ -1,10 +1,28 @@
+import React from 'react';
 import { GiShoppingCart } from "react-icons/gi";
-export default function Header() {
+import { Link } from 'react-router-dom';  
+import { IoBagHandle } from "react-icons/io5";
+import { FiLogOut } from "react-icons/fi";
+
+
+export default function Header({ user }) {
+    const userMode = user;
+
     return (
         <div className="header">
             <div className="title">
-                <GiShoppingCart className="cart-icon" />
-                <h4>Light's Shop</h4>
+                <IoBagHandle className="bag-icon" />
+                <h4>Just a Shop</h4>
+            </div>
+            <div className="icons">
+                {userMode === 'user' && (
+                    <Link to="/cart">
+                        <GiShoppingCart className="cart-icon" />
+                    </Link>
+                )}
+                <Link to="/">
+                    <FiLogOut className="logout-icon" />
+                </Link>
             </div>
         </div>
     );
